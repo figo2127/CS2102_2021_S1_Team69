bb12f17440e9de7a6028fb9469b6f97f255c10a4
dearvae <lovevae7@gmail.com>
2020-10-22 21:30:10 +0800

    test

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE petcaringdb;
 
--- CREATE TABLE todo(
---   todo_id SERIAL PRIMARY KEY,
---   description VARCHAR(255)
--- );
+CREATE TABLE todo(
+  todo_id SERIAL PRIMARY KEY,
+  description VARCHAR(255)
+);
