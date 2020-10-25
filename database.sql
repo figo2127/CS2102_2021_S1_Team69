a801fde3a4ef4bc620b5f62040fbbfaa798ba399
Jiaying <cooolnichole@gmail.com>
2020-10-25 21:16:56 +0800

    Table Creation with Data

--- database.sql
+++ database.sql
@@ -5,6 +5,8 @@ CREATE DATABASE petcaringdb;
 --   description VARCHAR(255)
 -- );
 
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS credit_card;
 
 CREATE TABLE owners(
     owner_userName VARCHAR(20) NOT NULL REFERENCES accounts(username) ON DELETE Cascade,
@@ -19,4 +21,20 @@ CREATE TABLE credit_card(
     PRIMARY KEY(owner_userName, credit_card_num)
 
 );
+
+INSERT INTO owners VALUES
+('adi'),
+('jy'),
+('dearvae'),
+('gycc'),
+('figo'),
+('zz');
+
+INSERT INTO credit_card VALUES
+('adi', 123456789333, 111, 2030-01-02),
+('jy', 123457890444, 222, 2020-11-11),
+('dearvae', 245373849333, 333, 2022-01-01),
+('gycc',435787654444, 444, 2023-01-04),
+('figo', 647234516211, 555, 2025-02-02),
+('zz',264829362516, 666, 2020-12-12);
 
