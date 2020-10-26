2d8af7a42fe89b3a3f7713668d4652198ceaf349
Yichao <guo7253@gmail.com>
2020-10-26 21:30:06 +0800

    yichao apis done

--- index.js
+++ index.js
@@ -10,10 +10,125 @@ app.use(express.json()); //req.body
 //ROUTES//
 app.get("/accounts", async (req, res) => {
   try {
-    const allAccounts = await pool.query("SELECT * FROM accounts");
+    const allAccounts = await pool.query(
+      "SELECT * FROM accounts"
+    );
     res.json(allAccounts.rows);
   } catch (err) {
-    console.error(ee.message);
+    console.log(err.message);
+  }
+})
+
+
+//create Account
+/**
+ * Need request body in format
+ * {
+ *  "username" : "activeuser",
+ *  "password" : "123456",
+ *  "name" : "yichao",
+ *  "phone" : "86561895",
+ *  "area" : "South",
+ *  "address" : "12 Kent Ridge Drive"
+ * }
+ * Note: area and address columns is optional
+ * return inserted data upon success
+ */
+app.post("/accounts", async (req, res) => {
+  try {
+    // const allAccounts = await pool.query("SELECT * FROM accounts");
+    // res.json(allAccounts.rows);
+    const {username} = req.body;
+    const {password} = req.body;
+    const {name} = req.body;
+    const {phone} = req.body;
+    const {area} = req.body;
+    const {address} = req.body;
+    const newAccount = await pool.query(
+      "INSERT INTO accounts (username, password, name, phone, area, address) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [username, password, name, phone, area, address]
+    );
+    res.json(newAccount.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+//login
+/**
+ * Need request body in format
+ * {
+ *  "username" : "activeuser",
+ *  "password" : "123456"
+ * }
+ * if success return corresponding database row
+ * else return string "Incorrect username or password"
+ */
+app.get("/accounts/login", async (req, res) => {
+  try {
+    const {username} = req.body;
+    const {password} = req.body;
+    const result = await pool.query(
+      "SELECT * FROM accounts WHERE username = $1 and password = $2",
+      [username, password]
+    );
+    if (result.rows.length == 0) {
+      res.send("Incorrect username or password");
+      // res.json({content: "Incorrect username or password"})
+    } else {
+      res.json(result.rows[0]);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+//getOwnerInfo
+/**
+ * need request body in format
+ * {
+ *  "username" : "activeuser"
+ * }
+ * return combined json result from accounts table and owners table
+ * Unchecked due to no owner table created yet
+ */
+app.get("/owners/getOwnerInfo", async (req, res) => {
+  try {
+    const {username} = req.body;
+    const result = await pool.query(
+      "SELECT * FROM owners WHERE username = $1",
+      [username]
+    );
+    const result2 = await pool.query(
+      "SELECT * FROM accounts WHERE username = $1",
+      [username]
+    );
+    const combinedResult = result.rows[0].concat(result2.rows[0]);
+    res.json(combinedResult);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+//update Owner Info
+/**
+ * Unchecked due to no owner table created yet
+ */
+app.put("/owners/updateOwnerInfo", async (req, res) => {
+  try {
+    const {username} = req.body;
+    const {password} = req.body;
+    const {name} = req.body;
+    const {phone} = req.body;
+    const {area} = req.body;
+    const {address} = req.body;
+    const updateOwnerInfo = await pool.query(
+      "UPDATE accounts SET password = $2, name = $3, phone = $4, area = $5, address = $6 WHERE username = $7",
+      [password, name, phone, area, address, username]
+    );
+    res.send("Owner info updated successfully")
+  } catch (err) {
+    console.error(err.message);
   }
 })
 
@@ -22,9 +137,23 @@ app.get("/admin", async (req, res) => {
     const allAdmin = await pool.query("SELECT * FROM admin");
     res.json(allAdmin.rows);
   } catch (err) {
-    console.error(ee.message);
+    console.error(err.message);
   }
 })
+
+// app.post("/accounts", async (req, res) => {
+//   try{
+//     const { desc } = req.body;
+//     console.log(desc);
+//     res.json(req);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// })
+
+/**
+ * Demo todo apis
+ */
 //create a todo
 
 app.post("/todos", async (req, res) => {
