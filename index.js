8b3d8b1fbe205ed08d1d21e820c9151ba666f94f
dearvae <lovevae7@gmail.com>
2020-10-27 17:59:20 +0800

    add category api

--- index.js
+++ index.js
@@ -38,12 +38,7 @@ app.post("/accounts", async (req, res) => {
   try {
     // const allAccounts = await pool.query("SELECT * FROM accounts");
     // res.json(allAccounts.rows);
-    const {username} = req.body;
-    const {password} = req.body;
-    const {name} = req.body;
-    const {phone} = req.body;
-    const {area} = req.body;
-    const {address} = req.body;
+    const { username, password, name, phone, area, address} = req.body;
     const newAccount = await pool.query(
       "INSERT INTO accounts (username, password, name, phone, area, address) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
       [username, password, name, phone, area, address]
@@ -66,8 +61,7 @@ app.post("/accounts", async (req, res) => {
  */
 app.get("/accounts/login", async (req, res) => {
   try {
-    const {username} = req.body;
-    const {password} = req.body;
+    const { username, password } = req.body;
     const result = await pool.query(
       "SELECT * FROM accounts WHERE username = $1 and password = $2",
       [username, password]
@@ -94,7 +88,7 @@ app.get("/accounts/login", async (req, res) => {
  */
 app.get("/owners/getOwnerInfo", async (req, res) => {
   try {
-    const {username} = req.body;
+    const { username } = req.body;
     const result = await pool.query(
       "SELECT * FROM owners WHERE username = $1",
       [username]
@@ -116,12 +110,8 @@ app.get("/owners/getOwnerInfo", async (req, res) => {
  */
 app.put("/owners/updateOwnerInfo", async (req, res) => {
   try {
-    const {username} = req.body;
-    const {password} = req.body;
-    const {name} = req.body;
-    const {phone} = req.body;
-    const {area} = req.body;
-    const {address} = req.body;
+    const { username } = req.params;
+    const { password, name, phone, area, address } = req.body;
     const updateOwnerInfo = await pool.query(
       "UPDATE accounts SET password = $2, name = $3, phone = $4, area = $5, address = $6 WHERE username = $7",
       [password, name, phone, area, address, username]
@@ -132,10 +122,10 @@ app.put("/owners/updateOwnerInfo", async (req, res) => {
   }
 })
 
-app.get("/admin", async (req, res) => {
+app.get("/admins", async (req, res) => {
   try {
-    const allAdmin = await pool.query("SELECT * FROM admin");
-    res.json(allAdmin.rows);
+    const allAdmins = await pool.query("SELECT * FROM admin");
+    res.json(allAdmins.rows);
   } catch (err) {
     console.error(err.message);
   }
@@ -151,77 +141,65 @@ app.get("/admin", async (req, res) => {
 //   }
 // })
 
-/**
- * Demo todo apis
- */
-//create a todo
-
-app.post("/todos", async (req, res) => {
-  try {
-    const { description } = req.body;
-    const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1) RETURNING *",
-      [description]
+//add a new category
+app.post("/categories", async (req, res) => {
+  try{
+    const { category_name, base_price } = req.body;
+    const newCategory = await pool.query(
+      "INSERT INTO categories (category_name, base_price) VALUES($1, $2) RETURNING *",
+      [category_name, base_price]
     );
+    res.json(newCategory.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
-    res.json(newTodo.rows[0]);
+
+//get all categories
+app.get("/categories", async (req, res) => {
+  try {
+    const allCategories = await pool.query("SELECT * FROM categories;");
+    res.json(allCategories.rows);
   } catch (err) {
     console.error(err.message);
   }
 });
 
-//get all todos
-
-app.get("/todos", async (req, res) => {
+//get a category by category_name
+app.get("/categories/:category_name", async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM todo");
-    res.json(allTodos.rows);
+    const { category_name } = req.params;
+    const category = await pool.query("SELECT * FROM categories WHERE category_name = $1", [category_name]);
+    res.json(category.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 
-//get a todo
-
-app.get("/todos/:id", async (req, res) => {
+//update a category
+app.put("/categories/:category_name", async (req, res) => {
   try {
-    const { id } = req.params;
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
-      id
-    ]);
-
-    res.json(todo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//update a todo
-
-app.put("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { description } = req.body;
-    const updateTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id = $2",
-      [description, id]
+    const { category_name  } = req.params;
+    const { base_price } = req.body;
+    const updateCategory = await pool.query(
+      "UPDATE category SET base_price = $1 WHERE category_name = $2",
+      [base_price, category_name]
     );
-
-    res.json("Todo was updated!");
+    res.json("Category was updated!");
   } catch (err) {
     console.error(err.message);
   }
 });
 
-//delete a todo
-
-app.delete("/todos/:id", async (req, res) => {
+//delete a category
+app.delete("/categories/:category_name", async (req, res) => {
   try {
-    const { id } = req.params;
-    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
-      id
+    const { category_name } = req.params;
+    const deleteCategory = await pool.query("DELETE FROM category WHERE category_name = $1", [
+      category_name
     ]);
-    res.json("Todo was deleted!");
+    res.json("Category was deleted!");
   } catch (err) {
     console.log(err.message);
   }
