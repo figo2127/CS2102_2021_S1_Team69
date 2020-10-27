7b6605e49f66d8cd314162a6ff0fcc76b208faa4
Jiaying <cooolnichole@gmail.com>
2020-10-27 22:51:53 +0800

    apis

--- database.sql
+++ database.sql
@@ -1,26 +1,103 @@
 CREATE DATABASE petcaringdb;
 
--- CREATE TABLE todo(
---   todo_id SERIAL PRIMARY KEY,
---   description VARCHAR(255)
--- );
 
-DROP TABLE IF EXISTS owners;
-DROP TABLE IF EXISTS credit_card;
+CREATE TABLE accounts(
+	username VARCHAR(20) NOT NULL,
+	password VARCHAR(20) NOT NULL,
+	name VARCHAR(30) NOT NULL,
+	phone VARCHAR(20) NOT NULL,
+	area VARCHAR(5),
+	address VARCHAR(80),
+	PRIMARY KEY(username)
+);
+
+CREATE TABLE admins(
+	account_userName VARCHAR(20) NOT NULL REFERENCES accounts(username),
+	PRIMARY KEY(account_username)
+);
 
 CREATE TABLE owners(
     owner_userName VARCHAR(20) NOT NULL REFERENCES accounts(username) ON DELETE Cascade,
 	PRIMARY KEY(owner_userName)
 );
 
+CREATE TABLE categories (
+	category_name VARCHAR(20) PRIMARY KEY,
+	base_price NUMERIC(5,2) NOT NULL
+);
+
+CREATE TABLE pets (
+	pname VARCHAR(20) NOT NULL,
+	owner_username VARCHAR(20) NOT NULL REFERENCES owners(owner_userName) ON DELETE CASCADE,
+	requirements TEXT,
+	belongs VARCHAR(20) NOT NULL REFERENCES categories(category_name),
+	PRIMARY KEY(pname, owner_username)
+);
+
 CREATE TABLE credit_card(
     owner_userName VARCHAR(20) REFERENCES owners(owner_userName) ON DELETE Cascade,
-    credit_card_num INTEGER NOT NULL,
+    credit_card_num VARCHAR(16) NOT NULL,
     cvv INTEGER NOT NULL,
-    expiry_dade DATE NOT NULL,
+    expiry_date DATE NOT NULL,
     PRIMARY KEY(owner_userName, credit_card_num)
 
 );
+
+CREATE TABLE carers (
+	username VARCHAR(20) NOT NULL REFERENCES accounts(username),
+	category VARCHAR(20) NOT NULL REFERENCES categories(category_name),
+	number_pets INT,
+	rating NUMERIC(3, 2),
+	isFullTime BOOL,
+	PRIMARY KEY(username)
+);
+
+CREATE TABLE availability (
+	available_date timestamp(0) NOT NULL,
+	carer_username VARCHAR(20) NOT NULL REFERENCES carers(username),
+	PRIMARY KEY(available_date, carer_username)
+	-- ON DELETE CASCAdssasasa
+);
+
+CREATE TABLE availability_bid (
+	available_date timestamp(0) NOT NULL,
+	carer_username VARCHAR(20) NOT NULL REFERENCES carers(username),
+    owner_username VARCHAR(20) NOT NULL,
+    pname VARCHAR(20) NOT NULL,
+    FOREIGN KEY (owner_username, pname) REFERENCES pets(owner_userName, pname),
+	PRIMARY KEY(available_date, carer_username)
+);
+
+-- bids table
+CREATE TABLE bids(
+  carer_username VARCHAR(20) NOT NULL REFERENCES carers(username),
+  owner_username VARCHAR(20) NOT NULL REFERENCES owners(owner_userName),
+  pname VARCHAR(20) NOT NULL,
+  bid_date DATE NOT NULL,
+  price NUMERIC NOT NULL,
+  is_sucessful INTEGER NULL,
+  credit_card_num VARCHAR(16),
+  payment_date DATE NOT NULL,
+  payment_mode VARCHAR(50) NOT NULL,
+  delivery_method VARCHAR(50) NOT NULL,
+  review_rating INTEGER NULL,
+  review_content VARCHAR(500) NULL,
+  review_date DATE NOT NULL,
+  PRIMARY KEY(carer_username, owner_username, pname),
+  FOREIGN KEY (owner_username, pname) REFERENCES pets(owner_userName, pname)
+);
+
+INSERT INTO accounts VALUES 
+('gycc', '123456', 'Guo Yichao', '86561895', 'West', 'Temasek Hall, 12 Kent Ridge Drive'),
+('dearvae', '123456', 'Li Beining', '83940829', 'North', '26 Clementi Road'),
+('adi', '123456', 'Adi Yoga', '98394292', 'West', '35 Clementi Road'),
+('zz', '123456', 'Zheng Zhi', '92839040', 'North', '23 Bedok Central'),
+('figo', '123456', 'Lee Ze Xin', '82038401', 'South', 'PGP'),
+('jy', '123456', 'Jiaying', '91084982', 'South', 'PGP');
+
+INSERT INTO admins VALUES
+('adi'),
+('zz');
 
 INSERT INTO owners VALUES
 ('adi'),
@@ -31,10 +108,27 @@ INSERT INTO owners VALUES
 ('zz');
 
 INSERT INTO credit_card VALUES
-('adi', 123456789333, 111, 2030-01-02),
-('jy', 123457890444, 222, 2020-11-11),
-('dearvae', 245373849333, 333, 2022-01-01),
-('gycc',435787654444, 444, 2023-01-04),
-('figo', 647234516211, 555, 2025-02-02),
-('zz',264829362516, 666, 2020-12-12);
+('adi', '1234567893331111', 111, '2030-01-02'),
+('jy', '1234578904441111', 222, '2020-11-11'),
+('dearvae', '2453738493331111', 333, '2022-01-01'),
+('gycc','4357876544441111', 444, '2023-01-04'),
+('figo', '6472345162111111', 555, '2025-02-02'),
+('zz','2648293625161111', 666, '2020-12-12');
 
+INSERT INTO categories VALUES
+('Bird', 40.00),
+('Cat', 50.00),
+('Chinchilla', 40.00),
+('Dog', 50.00),
+('Fish', 35.00),
+('Frog', 35.00),
+('Guinea Pig', 45.00),
+('Hamster', 30.00),
+('Mouse', 30.00),
+('Pig', 50.00),
+('Rabbit', 40.00),
+('Rat', 30.00),
+('Snake', 80.00),
+('Spider', 75.00),
+('Turtle', 30.00),
+('Tortoise', 30.00);
--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const pool = require("./db");
 
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+})
 //middleware
-// app.use(cors());
+app.use(cors());
 app.use(express.json()); //req.body
 
 //ROUTES//
 
 //create a todo
 
-app.post("/todos", async (req, res) => {
+// app.post("/todos", async (req, res) => {
+//   try {
+//     const { description } = req.body;
+//     const newTodo = await pool.query(
+//       "INSERT INTO todo (description) VALUES($1) RETURNING *",
+//       [description]
+//     );
+
+//     res.json(newTodo.rows[0]);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+app.post("/pets", async (req, res) => {
   try {
-    const { description } = req.body;
-    const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1) RETURNING *",
-      [description]
+    const { pname } = req.body;
+    const { owner_username } = req.body;
+    const { requirements } = req.body;
+    const { belongs } = req.body;
+    const newPet = await pool.query(
+      "INSERT INTO pets (pname, owner_username, requirements, belongs) VALUES($1, $2, $3, $4) RETURNING *",
+      [pname, owner_username, requirements, belongs]
     );
 
-    res.json(newTodo.rows[0]);
+    res.json(newPet.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
@@ -27,10 +47,19 @@ app.post("/todos", async (req, res) => {
 
 //get all todos
 
-app.get("/todos", async (req, res) => {
+// app.get("/todos", async (req, res) => {
+//   try {
+//     const allTodos = await pool.query("SELECT * FROM todo");
+//     res.json(allTodos.rows);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+app.get("/pets", async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM todo");
-    res.json(allTodos.rows);
+    const allPets = await pool.query("SELECT * FROM pets");
+    res.json(allPets.rows);
   } catch (err) {
     console.error(err.message);
   }
@@ -38,14 +67,27 @@ app.get("/todos", async (req, res) => {
 
 //get a todo
 
-app.get("/todos/:id", async (req, res) => {
+// app.get("/todos/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+//       id
+//     ]);
+
+//     res.json(todo.rows[0]);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+app.get("/pets/:pname", async (req, res) => {
   try {
-    const { id } = req.params;
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
-      id
+    const { pname } = req.params;
+    const pet = await pool.query("SELECT * FROM pets WHERE pname = $1", [
+      pname
     ]);
 
-    res.json(todo.rows[0]);
+    res.json(pet.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
@@ -53,16 +95,33 @@ app.get("/todos/:id", async (req, res) => {
 
 //update a todo
 
-app.put("/todos/:id", async (req, res) => {
+// app.put("/todos/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { description } = req.body;
+//     const updateTodo = await pool.query(
+//       "UPDATE todo SET description = $1 WHERE todo_id = $2",
+//       [description, id]
+//     );
+
+//     res.json("Todo was updated!");
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+app.put("/pets/:pname", async (req, res) => {
   try {
-    const { id } = req.params;
-    const { description } = req.body;
-    const updateTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id = $2",
-      [description, id]
+    const { pname } = req.params;
+    const { owner_username } = req.body;
+    const { requirements } = req.body;
+    const { belongs } = req.body;
+    const updatePet = await pool.query(
+      "UPDATE pets SET owner_username = $1, requirements = $2, belongs = $3 WHERE pname = $4",
+      [owner_username, requirements, belongs, pname]
     );
 
-    res.json("Todo was updated!");
+    res.json("Pet was updated!");
   } catch (err) {
     console.error(err.message);
   }
@@ -70,18 +129,27 @@ app.put("/todos/:id", async (req, res) => {
 
 //delete a todo
 
-app.delete("/todos/:id", async (req, res) => {
+// app.delete("/todos/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+//       id
+//     ]);
+//     res.json("Todo was deleted!");
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// });
+
+app.delete("/pets/:pname", async (req, res) => {
   try {
-    const { id } = req.params;
-    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
-      id
+    const { pname } = req.params;
+    const deletePet = await pool.query("DELETE FROM pets WHERE pname = $1", [
+      pname
     ]);
-    res.json("Todo was deleted!");
+    res.json("Pet was deleted!");
   } catch (err) {
     console.log(err.message);
   }
 });
 
-app.listen(5000, () => {
-  console.log("server has started on port 5000");
-});
