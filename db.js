3bd9ae1dc2d64948d6c221d4a46fe83c2eeb7f90
Wong-ZZ <wongzhengzhi@gmail.com>
2020-11-06 19:38:07 +0800

    Change db.js to accept environment variables if it exists

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
+  user: process.env.DB_USERNAME || "postgres",
+  password: process.env.DB_USERNAME || "root",
+  host: process.env.DB_URL || "localhost",
   port: 5432,
-  database: "petcaringdb"
+  database: process.env.DB_NAME || "petcaringdb"
 });
 
 module.exports = pool;
