0f925e8dc56e61ad1ddee7de3fdd8b3b34a31fab
Wong-ZZ <wongzhengzhi@gmail.com>
2020-11-06 20:02:07 +0800

    Fix environment variable naming

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: process.env.DB_USERNAME || "postgres",
-  password: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root",
   host: process.env.DB_URL || "localhost",
   port: 5432,
   database: process.env.DB_NAME || "petcaringdb"
