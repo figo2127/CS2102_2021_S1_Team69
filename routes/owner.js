21875b2a035628340d5790466ddd1fa4fe0c16a5
Wong-ZZ <wongzhengzhi@gmail.com>
2020-11-07 23:16:37 +0800

    Readd route for getting owner's bid history

--- routes/owner.js
+++ routes/owner.js
@@ -55,47 +55,14 @@ router.get("/reviews/:owner_name", authUser, async (req, res) => {
   }
 });
 
-
-// get list of sucess bids by owner name 
-// router.post("/bids/:owner_name", async (req, res) => {
-//   try {
-//     const { year, month, carer_name, is_fulltime } = req.body;
-//     const start_of_month = `${year}-${month}-01`
-//     let base_pay = 0.0
-//     let portion = 0.75;
-//     let offset = 0;
-//     if (is_fulltime) {
-//       base_pay = 3000.0
-//       portion = 0.80;
-//       offset = 60;
-//     }
-//     let query =
-//       `SELECT (SUM (b.daily_price) OVER ()) * $1 + $2 AS salary
-//       FROM (
-//           SELECT generate_series(
-//             $3,
-//             (DATE($3) + INTERVAL '1 month' - INTERVAL '1 day')::DATE,
-//             '1 day'::interval
-//           )::date AS day
-//       ) days_in_month
-//       CROSS JOIN bids b
-//       WHERE
-//         b.start_date <= day AND b.end_date >= day AND
-//         b.carer_name = $4
-//       ORDER BY
-//         day ASC,
-//         daily_price ASC
-//       OFFSET $5;`;
-//     const getSalary = await pool.query(query, [portion, base_pay, start_of_month, carer_name, offset]);
-//     if (getSalary.rows[0]) {
-//       res.json(getSalary.rows[0]);
-//     } else {
-//       res.json({ salary: "3000.00" });
-//     }
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// })
-
+router.get("/bids/:owner_name", async (req, res) => {
+  try {
+    const { owner_name } = req.params;
+    const getBids = await pool.query(`SELECT * FROM bids WHERE owner_name = $1;`, [owner_name]);
+    res.json(getBids.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+})
 
 module.exports = router;
